0e4a2788179498252ffa463666892b8a1ed9a5c8
kavishkagihan <85458014+kavishkagihan@users.noreply.github.com>
2023-02-09 20:42:23 +0530

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,13 @@
+echo "Downloading GlazeWM from the releases"
+curl https://github.com/lars-berger/GlazeWM/releases/download/v1.11.1/GlazeWM_x64_1.11.1.exe  -O glazeWM.exe
+
+echo "Copying the example config"
+mkdir ~\.glaze-wm\
+copy config.yaml ~\.glaze-wm\
+
+echo "Enabling GlazeWM and AutoHotKeys to run in startup"
+copy glazeWM.exe "~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+copy AutoHotKey-Scripts\* "~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+echo "Starting Glaze"
+.\glazeWM.exe
